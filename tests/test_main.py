d516b74cf9cd31fefb2b51f486c413a77601f6a1
erickummelstedt <138875214+erickummelstedt@users.noreply.github.com>
2025-04-09 16:20:38 +0200

    fix: updated file paths in main.py, test.py, and utils.py to resolve import issues and ensure correct module referencing

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 import sys
 
 # home_dir = os.path.expanduser('~')
+# local_path = '/home/erickummelstedt/lecodebase/ubiquitinformatics/src/main.py'
 local_path = '/Users/ekummelstedt/le_code_base/ubiquitinformatics'
 sys.path.insert(0, local_path)
 
